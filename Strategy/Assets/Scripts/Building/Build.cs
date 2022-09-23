ae1927cff4308b3cc453c0f58ed708be39a055f1
Alexander <102901721+Labu1ay@users.noreply.github.com>
2022-09-23 08:53:49 +0300

    BuildingBarackAndMine

---
+++ Strategy/Assets/Scripts/Building/Build.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Build : SelectbleObject {
+    public int Price;
+    public int XSize = 3;
+    public int ZSize = 3;
+
+    private void OnDrawGizmos() {
+        float cellSize = FindObjectOfType<BuildingPlacer>().CellSize;
+
+        for (int x = 0; x < XSize; x++) {
+            for (int z = 0; z < ZSize; z++) {
+                Gizmos.DrawWireCube(transform.position + new Vector3(x, 0, z) * cellSize, new Vector3(1f, 0f, 1f) * cellSize);
+            }
+        }
+       
+    }
+}
